2bd083c221d8c037e91ea4e33793f2e3305898a1
sunyuxuan <1326170446@qq.com>
2021-11-26 06:45:38 +0800

    增加第一节课作业文件

---
+++ 00.hello/homework.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/**
+编译第一个 golang 小程序：你好，golang
+把一首诗写到 golang 小程序
+输出一颗五角星
+*/
+func main() {
+	fmt.Println("你好，golang")
+
+	fmt.Println("	  鸟鸣涧")
+	fmt.Println("		王维 〔唐代〕")
+	fmt.Print("人闲桂花落，")
+	fmt.Println("夜静春山空。")
+	fmt.Print("月出惊山鸟，")
+	fmt.Println("时鸣春涧中。")
+
+	fmt.Println("  	    *	")
+	fmt.Println(" 	   ***")
+	fmt.Println("	  *****	")
+	fmt.Println("******************")
+	fmt.Println("    ********	")
+	fmt.Println("   ****  ****	")
+	fmt.Println("  *          *	")
+}
