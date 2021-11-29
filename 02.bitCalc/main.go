63b190b959d9ddfd4a621829fd630119c541e706
syxboss <46069110+syxboss@users.noreply.github.com>
2021-11-30 06:25:54 +0800

    11月28日 第二节课笔记

---
+++ 02.bitCalc/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 100, 31
+	fmt.Println(a ^ b) // 异或的交换律
+	fmt.Println(b ^ a)
+
+	// 查找一堆数中的唯一不重复的值
+	arr := []int{4, 3, 4, 5, 6, 7, 5, 6, 3}
+	result := -1
+	for _, item := range arr {
+		if result < 0 {
+			result = item
+		} else {
+			result = result ^ item
+		}
+	}
+	fmt.Println(result)
+
+}
