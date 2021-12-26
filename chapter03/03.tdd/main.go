3a6d7c5d1b78bd58ca0b10b6cc12275cb4840b55
syxboss <46069110+syxboss@users.noreply.github.com>
2021-12-26 10:42:54 +0800

    完成tdd案例1的代码实现

--- chapter03/03.tdd/main.go
+++ chapter03/03.tdd/main.go
@@ -1,3 +1,32 @@
 package main
 
-main
+import "sort"
+
+var (
+	personFatRate = map[string]float64{}
+)
+
+func inputRecord(name string, fatRate float64) {
+	personFatRate[name] = fatRate
+}
+
+func getRand(name string) (rank int, fatRate float64) {
+	fatRate2PersonMap := map[float64]string{}
+	rankArr := make([]float64, 0, len(personFatRate))
+	// 反转 k v
+	for nameItem, frItem := range personFatRate {
+		fatRate2PersonMap[frItem] = nameItem
+		rankArr = append(rankArr, frItem)
+	}
+	// 排序 sort
+	sort.Float64s(rankArr)
+	for i, frItem := range rankArr {
+		_name := fatRate2PersonMap[frItem]
+		if _name == name {
+			rank = i + 1
+			fatRate = frItem
+			return
+		}
+	}
+	return 0, 0
+}
