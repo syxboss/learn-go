f6a10e58c0007ee733d56976d6a7193782a12f65
syxboss <46069110+syxboss@users.noreply.github.com>
2021-12-26 10:54:58 +0800

    完成tdd案例2、3的代码实现

--- chapter03/03.tdd/main.go
+++ chapter03/03.tdd/main.go
@@ -1,31 +1,42 @@
 package main
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 var (
 	personFatRate = map[string]float64{}
 )
 
-func inputRecord(name string, fatRate float64) {
-	personFatRate[name] = fatRate
+func inputRecord(name string, fatRate ...float64) {
+	minFatRate := math.MaxFloat64
+	for _, item := range fatRate {
+		if minFatRate > item {
+			minFatRate = item
+		}
+	}
+	personFatRate[name] = minFatRate
 }
 
 func getRand(name string) (rank int, fatRate float64) {
-	fatRate2PersonMap := map[float64]string{}
+	fatRate2PersonMap := map[float64][]string{} // 案例2 改为数组
 	rankArr := make([]float64, 0, len(personFatRate))
 	// 反转 k v
 	for nameItem, frItem := range personFatRate {
-		fatRate2PersonMap[frItem] = nameItem
+		fatRate2PersonMap[frItem] = append(fatRate2PersonMap[frItem], nameItem)
 		rankArr = append(rankArr, frItem)
 	}
 	// 排序 sort
 	sort.Float64s(rankArr)
 	for i, frItem := range rankArr {
-		_name := fatRate2PersonMap[frItem]
-		if _name == name {
-			rank = i + 1
-			fatRate = frItem
-			return
+		_names := fatRate2PersonMap[frItem]
+		for _, _name := range _names {
+			if _name == name {
+				rank = i + 1
+				fatRate = frItem
+				return
+			}
 		}
 	}
 	return 0, 0
