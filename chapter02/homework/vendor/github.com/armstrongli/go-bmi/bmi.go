04fe709138ad4fb46784dacc436e7f9b5862850f
syxboss <46069110+syxboss@users.noreply.github.com>
2021-12-28 13:46:41 +0800

    提交第二次作业

---
+++ chapter02/homework/vendor/github.com/armstrongli/go-bmi/bmi.go
@@ -0,0 +1,19 @@
+package go_bmi
+
+import "fmt"
+
+func BMI(weight, height float64) (bmi float64, err error) {
+	fmt.Println("以下是在本地扩展过的BMI方法")
+	if weight <= 0 {
+		err =fmt.Errorf("weight cannot be negative or zero!")
+		return
+	}
+	if height <= 0 {
+		err = fmt.Errorf("height cannot be negative or zero!")
+		return
+	}
+	fmt.Println("计算BMI")
+	bmi = weight / (height * height)
+	fmt.Println("计算BMI结束，BMI：" ,bmi)
+	return
+}
